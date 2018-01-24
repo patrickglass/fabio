9c8a86aed9c7fcebd9df26937b626912a8b04f0b
Patrick Glass <patrick.glass@sap.com>
2018-01-24 11:38:11 -0800

    ServiceID as per Consul docs only required unique per host
    Update the health checks to ensure ServiceIDs do not get routed to a dead node if the serviceID is healthy on another node.
    This implementation will ensure service health includes serfHealth as well as CheckID health.

--- registry/consul/passing_test.go
+++ registry/consul/passing_test.go
@@ -11,8 +11,11 @@ func TestPassingServices(t *testing.T) {
 	var (
 		serfPass      = &api.HealthCheck{Node: "node", CheckID: "serfHealth", Status: "passing"}
 		serfFail      = &api.HealthCheck{Node: "node", CheckID: "serfHealth", Status: "critical"}
+		serfNode2Pass = &api.HealthCheck{Node: "node2", CheckID: "serfHealth", Status: "passing"}
+		serfNode2Fail = &api.HealthCheck{Node: "node2", CheckID: "serfHealth", Status: "critical"}
 		svc1Pass      = &api.HealthCheck{Node: "node", CheckID: "service:abc", Status: "passing", ServiceName: "abc", ServiceID: "abc-1"}
 		svc1Node2Pass = &api.HealthCheck{Node: "node2", CheckID: "service:abc", Status: "passing", ServiceName: "abc", ServiceID: "abc-1"}
+		svc1Node2Fail = &api.HealthCheck{Node: "node2", CheckID: "service:abc", Status: "critical", ServiceName: "abc", ServiceID: "abc-1"}
 		svc1Warn      = &api.HealthCheck{Node: "node", CheckID: "service:abc", Status: "warning", ServiceName: "abc", ServiceID: "abc-2"}
 		svc1Crit      = &api.HealthCheck{Node: "node", CheckID: "service:abc", Status: "critical", ServiceName: "abc", ServiceID: "abc-3"}
 		svc2Pass      = &api.HealthCheck{Node: "node", CheckID: "my-check-id", Status: "passing", ServiceName: "def", ServiceID: "def-1"}
@@ -30,6 +33,10 @@ func TestPassingServices(t *testing.T) {
 		{[]string{"passing"}, []*api.HealthCheck{svc1Pass}, []*api.HealthCheck{svc1Pass}},
 		{[]string{"passing"}, []*api.HealthCheck{svc1Pass, svc2Pass}, []*api.HealthCheck{svc1Pass, svc2Pass}},
 		{[]string{"passing"}, []*api.HealthCheck{serfPass, svc1Pass}, []*api.HealthCheck{svc1Pass}},
+		{[]string{"passing"}, []*api.HealthCheck{serfPass, serfNode2Pass, svc1Pass, svc1Node2Pass}, []*api.HealthCheck{svc1Pass, svc1Node2Pass}},
+		{[]string{"passing"}, []*api.HealthCheck{serfPass, serfNode2Pass, svc1Pass, svc1Node2Fail}, []*api.HealthCheck{svc1Pass}},
+		{[]string{"passing"}, []*api.HealthCheck{serfPass, serfNode2Fail, svc1Pass, svc1Node2Pass}, []*api.HealthCheck{svc1Pass}},
+		{[]string{"passing"}, []*api.HealthCheck{serfFail, serfNode2Pass, svc1Pass, svc1Node2Pass}, []*api.HealthCheck{svc2Pass}},
 		{[]string{"passing"}, []*api.HealthCheck{serfFail, svc1Pass}, nil},
 		{[]string{"passing"}, []*api.HealthCheck{nodeMaint, svc1Pass}, nil},
 		{[]string{"passing"}, []*api.HealthCheck{svc1Maint, svc1Pass}, nil},
